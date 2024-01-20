798d029c745439487ddc057789eac7aa35f4daa4
intersalad <140984613+intersalad@users.noreply.github.com>
2024-01-21 00:39:30 +0300

    Update bot.mjs

--- src/bot.mjs
+++ src/bot.mjs
@@ -2,6 +2,8 @@ import TeleBot from "telebot"
 
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
-bot.on("text", msg => msg.reply.text(msg.text))
+bot.on("text", msg => msg.reply.text(msg))
+
+
 
 export default bot
