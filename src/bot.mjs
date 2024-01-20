19ad1cb23796e8599c8a1b696868f4d127012e66
intersalad <140984613+intersalad@users.noreply.github.com>
2024-01-21 00:50:08 +0300

    1

--- src/bot.mjs
+++ src/bot.mjs
@@ -4,6 +4,8 @@ const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
 bot.on("text", msg => msg.reply.text(msg.text))
 
+bot.on('/hello', (msg) => msg.reply.text(msg));
+
 bot.on('/start', (msg) => msg.reply.photo('https://picsum.photos/1000'));
 
 export default bot
