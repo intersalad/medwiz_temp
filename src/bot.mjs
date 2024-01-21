73ef367505e153dd548b1f6fb6776e068c38dce0
intersalad <140984613+intersalad@users.noreply.github.com>
2024-01-21 23:59:04 +0300

    try4

--- src/bot.mjs
+++ src/bot.mjs
@@ -7,7 +7,7 @@ bot.on(["text", "voice"], ctx => {
 });
 
 bot.on("forward", ctx => {
-  return bot.sendMessage(-1002116816322, `example: \n ${ctx.msg}`);
+  return bot.sendMessage(363625457, `example: \n ${ctx.text}`);
 });
 
 
