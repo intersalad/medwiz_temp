cb65fe13d689b6f04fa1423bf83425bae722df09
intersalad <140984613+intersalad@users.noreply.github.com>
2024-01-23 02:27:07 +0300

    gogo

--- src/bot.mjs
+++ src/bot.mjs
@@ -3,11 +3,14 @@ import TeleBot from "telebot"
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
 bot.on(["text", "voice"], ctx => {
+  if (ctx.chat.id != -1002116816322) {
   return bot.sendMessage(-1002116816322, `#${ctx.chat.id} \n ${ctx.text}`);
+  }
 });
 
+
 bot.on("forward", ctx => {
-  return bot.sendMessage(363625457, ctx.text);
+  return bot.sendMessage(363625457, "hi");
 });
 
 bot.on('/start', (msg) => msg.reply.photo('https://picsum.photos/1000'));
