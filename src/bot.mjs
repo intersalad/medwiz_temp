a767bf800b52ac9f81ed893b5e0346585ff2b57b
intersalad <140984613+intersalad@users.noreply.github.com>
2024-01-23 06:06:48 +0300

    cx

--- src/bot.mjs
+++ src/bot.mjs
@@ -5,20 +5,20 @@ const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
 bot.on(["text"], ctx => {
   if (ctx.chat.id != -1002116816322 && ctx.chat.id != -1002090103134) {
-  return bot.sendMessage(-1002116816322, `${ctx.chat.id} Открыто \n ${ctx.text}`);
+  return bot.sendMessage(-1002116816322, `${ctx.chat.id} Открыт \n ${ctx.text}`);
 }
 });
 
 
 bot.on(["voice"], ctx => {
   if (ctx.chat.id != -1002116816322 && ctx.chat.id != -1002090103134) {
-  return bot.sendVoice(-1002116816322, ctx.voice.file_id, { caption: `${ctx.chat.id} Открыто` });
+  return bot.sendVoice(-1002116816322, ctx.voice.file_id, { caption: `${ctx.chat.id} Открыт` });
 }
 });
 
 bot.on(["photo"], ctx => {
   if (ctx.chat.id != -1002116816322 && ctx.chat.id != -1002090103134) {
-    return bot.sendPhoto(-1002116816322, ctx.photo[0].file_id, { caption: `${ctx.chat.id} Открыто` })
+    return bot.sendPhoto(-1002116816322, ctx.photo[0].file_id, { caption: `${ctx.chat.id} Открыт` })
 }})
 
 
@@ -28,6 +28,12 @@ bot.on(["photo"], ctx => {
 bot.on("text", ctx => {
   if (ctx.chat.id == -1002090103134) {
     return bot.sendMessage(ctx.reply_to_message.text.split(" ")[0], ctx.text)
+  }
+})
+
+bot.on("photo", ctx => {
+  if (ctx.chat.id == -1002090103134) {
+    return bot.sendPhoto(ctx.reply_to_message.text.split(" ")[0], ctx.photo[0].file_id)
   }
 })
 
