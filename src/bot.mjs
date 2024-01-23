5905cfab10766fa5e9e97f31cc9b94ec7138d129
intersalad <140984613+intersalad@users.noreply.github.com>
2024-01-23 08:16:31 +0300

    m

--- src/bot.mjs
+++ src/bot.mjs
@@ -6,7 +6,7 @@ const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
 
 
-bot.on(["text", "photo", "voice", "video", "video_note", "sticker", "document"], ctx => {
+bot.on(["text", "photo", "voice", "video", "videonote", "sticker", "document"], ctx => {
   if (ctx.chat.id != -1002116816322 && ctx.chat.id != -1002090103134) {
     if (ctx.caption) {
         if (ctx.photo) {
