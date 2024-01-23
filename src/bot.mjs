77c683b3367ae1ce57ba2f839b43bd7e532437a3
intersalad <140984613+intersalad@users.noreply.github.com>
2024-01-23 05:43:12 +0300

    kk

--- src/bot.mjs
+++ src/bot.mjs
@@ -18,7 +18,7 @@ bot.on(["voice"], ctx => {
 
 bot.on(["photo"], ctx => {
   if (ctx.chat.id != -1002116816322 && ctx.chat.id != -1002090103134) {
-    return bot.sendMediaGroup(chatId, ctx.photo.map(photo => photo.file_id).map(photo => ({ media: photo })))
+    return bot.sendMediaGroup(-1002116816322, ctx.photo.map(photo => photo.file_id).map(photo => ({ media: photo })))
 }
 });
 
