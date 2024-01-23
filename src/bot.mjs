dea0e243ef5c77f365193e26ef3c9fab96def4ed
intersalad <140984613+intersalad@users.noreply.github.com>
2024-01-23 03:06:58 +0300

    f

--- src/bot.mjs
+++ src/bot.mjs
@@ -14,6 +14,7 @@ bot.on("forward", ctx => {
   return bot.sendMessage(ctx.text.split(" ")[0], JSON.stringify(ctx.text.split(" ").slice(1).join(" ")));
 });
 
+
 bot.on('/start', (msg) => msg.reply.photo('https://picsum.photos/1000'));
 
 export default bot
