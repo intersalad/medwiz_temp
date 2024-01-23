0504f141435b71e60023353ac7efb4a4f2144488
intersalad <140984613+intersalad@users.noreply.github.com>
2024-01-23 03:30:29 +0300

    1

--- src/bot.mjs
+++ src/bot.mjs
@@ -4,15 +4,13 @@ const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
 
 bot.on(["text", "voice"], ctx => {
-  if (ctx.chat.id != -1002116816322) {
+  if (ctx.chat.id != -1002116816322 && ctx.chat.id != -1002090103134) {
   return bot.sendMessage(-1002116816322, `${ctx.chat.id} \n ${ctx.text}`);
   }
 });
 
 
-bot.on("forward", ctx => {
-  return bot.sendMessage(363625457, JSON.stringify(ctx))
-});
+
 
 
 bot.on('/start', (msg) => msg.reply.photo('https://picsum.photos/1000'));
