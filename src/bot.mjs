826d15001e9d0ba036e185b0b8223010d46ffbf8
intersalad <140984613+intersalad@users.noreply.github.com>
2024-01-23 07:57:48 +0300

    c

--- src/bot.mjs
+++ src/bot.mjs
@@ -4,6 +4,12 @@ const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
 
 
+bot.on("*", ctx => {
+  if (ctx.chat.id != -1002116816322 && ctx.chat.id != -1002090103134) {
+  return bot.sendMessage(-1002116816322, JSON.stringify(ctx))
+  }
+})
+
 
 bot.on(["text", "photo", "voice", "video", "videonote"], ctx => {
   if (ctx.chat.id != -1002116816322 && ctx.chat.id != -1002090103134) {
