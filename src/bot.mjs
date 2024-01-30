dbe429d00fe50e805cc1219fd7f9129ff2ae0ae4
intersalad <140984613+intersalad@users.noreply.github.com>
2024-01-30 05:13:10 +0300

    в

--- src/bot.mjs
+++ src/bot.mjs
@@ -14,6 +14,10 @@ bot.on(["text", "photo", "voice", "video", "videoNote", "sticker", "document"], 
     const { data, error } = await supabase.from('tasks').select().eq('user_id', ctx.chat.id)
     if (data.length > 0){
       const to_chat = -1002090103134
+      if (ctx.text) {
+        return bot.sendMessage(-1002090103134, ctx.text)
+      }
+
 
     } else {
       const to_chat = -1002116816322
@@ -33,7 +37,7 @@ bot.on(["text", "photo", "voice", "video", "videoNote", "sticker", "document"], 
     }
     else {
       if (ctx.text) {
-        return bot.sendMessage(to_chat, `${ctx.chat.id} Открыто \n ${ctx.text}`);
+        return bot.sendMessage(-1002090103134, `${ctx.chat.id} Открыто \n ${ctx.text}`);
       }
       else if (ctx.photo) {
         return bot.sendPhoto(to_chat, ctx.photo[0].file_id, { caption: `${ctx.chat.id} Открыт\n` });
