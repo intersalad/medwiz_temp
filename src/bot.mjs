c818be1c615d49c1389e9fab9ff819a744b45f43
intersalad <140984613+intersalad@users.noreply.github.com>
2024-01-30 03:16:24 +0300

    v

--- src/bot.mjs
+++ src/bot.mjs
@@ -130,8 +130,8 @@ bot.on('/test', async (ctx) => {
     if (error) {
       throw error
     }
-    if (await supabase.from('tasks').select().eq('user_id', ctx.chat.id)["data"] != []){
-      return bot.sendMessage(ctx.chat.id, JSON.stringify(await supabase.from('tasks').select().eq('user_id', ctx.chat.id).data))
+    if (await supabase.from('tasks').select().eq('user_id', ctx.chat.id) != []){
+      return bot.sendMessage(ctx.chat.id, JSON.stringify(await supabase.from('tasks').select().eq('user_id', ctx.chat.id)))
     }
     else {
       return bot.sendMessage(ctx.chat.id, "false")
