22345559c562c6198be049c604b4bd1407e02a20
intersalad <140984613+intersalad@users.noreply.github.com>
2024-01-30 04:26:44 +0300

    м

--- src/bot.mjs
+++ src/bot.mjs
@@ -14,9 +14,11 @@ bot.on(["text", "photo", "voice", "video", "videoNote", "sticker", "document"], 
     const { data, error } = await supabase.from('tasks').select().eq('user_id', chatId)
     if (data.length > 0){
       const to_chat = -1002116816322
+      return bot.sendMessage(to_chat, "не добавлен")
     } else {
       const to_chat = -1002116816322
       const { error } = await supabase.from('tasks').insert({ user_id: ctx.chat.id })
+      return bot.sendMessage(to_chat, "новый добавлен")
     }
 
     if (ctx.caption) {
